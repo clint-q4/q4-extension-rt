cd14cf27d16100d50a1567dee97de37ab6e74285
Clinton <80900245+pravton@users.noreply.github.com>
2022-08-25 17:42:11 -0400

    initial react setup

---
+++ content.ts
@@ -0,0 +1,99 @@
+import type { PlasmoContentScript } from "plasmo"
+
+export const config: PlasmoContentScript = {
+  matches: ["<all_urls>"]
+}
+
+// window.addEventListener("load", () => {
+//   console.log("content script loaded")
+
+//   document.body.style.background = "pink"
+// })
+
+// Check if the current page is a q4site
+
+export const checkSite = (async () => {
+  let [currentTab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  console.log(currentTab);
+  chrome.scripting.executeScript({
+    target: { tabId: currentTab.id },
+    world: "MAIN",
+    func: checkQ4Site,
+  },
+  (result) => {
+    console.log(result);
+    for (let r of result) {
+      if(r.result) {
+        document.querySelector('#q4-site-verification').textContent = 'Q4 Site';
+      } else {
+        document.querySelector('#q4-site-verification').textContent = 'Sorry! Not a Q4 Site';
+        document.querySelector('.popup-buttons-container').style.display = 'none';
+      }
+    }
+  });
+
+  function checkQ4Site() {
+    let q4SiteVerification = false;
+    if(document.querySelector('.module-q4-credits')) {
+      q4SiteVerification = true;
+    } else if (document.location.href.includes('q4web')) {
+      q4SiteVerification = true;
+    }
+    return q4SiteVerification;
+  }
+});
+
+export const loginEditButtons = () => {
+  let loginButton = document.getElementById("q4-site-login-button");
+  let previewEditButton = document.getElementById("q4-site-preview-button");
+
+  loginButton.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: loginQ4Site,
+    });
+  });
+
+  previewEditButton.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: loadPreviewEditPage,
+    });
+  });
+}
+
+export const loginQ4Site = () => {
+  const baseUrl = `https://${location.hostname}`;  
+  if(baseUrl.includes("s4.q4web.com") && location.pathname.includes("preview/preview.aspx")) {
+    alert("You are already in a preview Site");
+  } else {
+    window.open(`${baseUrl}/login.aspx`);
+  }
+}
+
+export const loadPreviewEditPage = () => {
+  const base = `https://${location.hostname}`;
+  if (base.includes("s4.q4web.com") && location.pathname.includes("preview/preview.aspx")) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let id = urlParams.get("SectionId");
+    let sectionId = "f6d80133-c225-4a4e-aa62-d96e8cb2ac66";
+    const languageId = urlParams.get("LanguageId");
+    if (urlParams.get("PressReleaseId")) {
+      id = urlParams.get("PressReleaseId");
+      sectionId = "a9a908cb-21fa-46a6-8de1-05e23027c007";
+    } else if (urlParams.get("EventId")) {
+      id = urlParams.get("EventId");
+      sectionId = "a8a3d569-db57-40e4-b845-9fd6c267b0b3";
+    }
+    window.open(`${base}/admin/default.aspx?ItemID=${id}&LanguageId=${languageId}&SectionId=${sectionId}`, "_blank");
+  } else {
+    alert("This is not a Q4 Website Preview.");
+  }
+}
+
+// module.exports = { checkSite, loginEditButtons, loginQ4Site, loadPreviewEditPage };
--- popup.tsx
+++ popup.tsx
@@ -1,27 +1,56 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useStorage } from "@plasmohq/storage";
+import {checkSite} from "./content";
+import 'bulma/css/bulma.min.css';
+import './Style.css';
 
 function IndexPopup() {
   const [data, setData] = useState("")
-
+  useEffect(() => {
+    checkSite();
+  })
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="popup-container py-3 px-5">
+    <div className="popup-title-container">
+      <h2 className="is-size-2 has-text-weight-bold mb-2 has-text-centered">Site: <span id="q4-site-verification"></span></h2>
     </div>
+    <div className="popup-buttons-container">
+      <div className="popup-buttons-container-sublist" data-title="q4-cms-quick-links">
+        <h3 className="is-size-4 my-3">Quick CMS Links</h3>
+        <div className="buttons-container is-flex">
+          <div className="buttons is-flex is-3">
+            <button className="button is-link" id="q4-site-login-button">Login</button>
+          </div>
+          <div className="button-container">
+            <button className="button is-link" id="q4-site-preview-button">Edit Preview</button>
+          </div>
+          <div className="buttons is-flex is-3">
+            <button className="button is-link" id="q4-site-login-button">Login</button>
+          </div>
+          <div className="button-container">
+            <button className="button is-link" id="q4-site-preview-button">Edit Preview</button>
+          </div>
+        </div>
+      </div>
+      <div className="popup-buttons-container-sublist" data-title="quick-links">
+        <h3 className="is-size-4 my-3" id="target"></h3>
+        <div className="buttons-container is-flex">
+          <div className="buttons is-flex is-3">
+            <button className="button is-link" id="q4-site-login-button">Login</button>
+          </div>
+          <div className="button-container">
+            <button className="button is-link" id="q4-site-preview-button">Edit Preview</button>
+          </div>
+          <div className="buttons is-flex is-3">
+            <button className="button is-link" id="q4-site-login-button">Login</button>
+          </div>
+          <div className="button-container">
+            <button className="button is-link" id="q4-site-preview-button">Edit Preview</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
 
